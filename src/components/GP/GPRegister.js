464e066c433e74771080c8be4d26b384a0a94bc4
Danny Lee <siewkhee1990@hotmail.com>
2021-01-22 11:53:52 +0800

    GP registration completed

---
+++ src/components/GP/GPRegister.js
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import axios from "axios";
+import { BACKEND_URL_GPS } from "../Constant";
+import { Button, Form } from "react-bootstrap";
+
+function GPRegister() {
+  const [clinicName, setClinicName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [adminCode, setAdminCode] = useState("");
+
+  const register = (event) => {
+    event.preventDefault();
+    if (
+      !clinicName ||
+      !address ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !adminCode
+    ) {
+      alert("Please fill in required field!");
+    } else if (password === confirmPassword) {
+      axios
+        .post(`${BACKEND_URL_GPS}/create`, {
+          clinicName: clinicName,
+          address: address,
+          email: email,
+          password: password,
+          adminCode: adminCode,
+        })
+        .then((res) => {
+          alert(res.data.message);
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    } else {
+      alert("Confirm password and password not matched!");
+    }
+  };
+
+  return (
+    <div>
+      <h1>GP Register</h1>
+      <Form onSubmit={(event) => register(event)} className="m-3">
+        <Form.Group controlId="clinicname">
+          <Form.Label>Clinic Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Clinic's name"
+            required
+            value={clinicName}
+            onChange={(event) => setClinicName(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="address">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type="address"
+            placeholder="last name"
+            required
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="password(8 characters)"
+            required
+            minLength="8"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="confirmpassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="confirm password"
+            required
+            minLength="8"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="admincode">
+          <Form.Label>Admin Code</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Admin code"
+            required
+            value={adminCode}
+            onChange={(event) => setAdminCode(event.target.value)}
+          />
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default GPRegister;
