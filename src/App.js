2d5806fef4e532b21b155ee2e42c943ecd59a40b
VikiBoi <43218183+VikiBoi@users.noreply.github.com>
2021-01-22 14:19:50 +0800

    Dashboard in progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Main from "./components/Main";
 import PatientLogin from "./components/Patient/PatientLogin";
 import GPLogin from "./components/GP/GPLogin";
 import GPDashboard from "./components/GP/GPDashboard";
+import PTDashboard from "./components/Patient/PTDashboard";
+
 
 function App() {
   return (
@@ -14,7 +16,8 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route path="/patient" component={PatientLogin} />
         <Route path="/gp" component={GPLogin} />
-        <Route path="/patient/dashboard" component={GPDashboard} />
+        <Route path="/gp/dashboard" component={GPDashboard} />
+        <Route path="/patient/ptdashboard" component={PTDashboard} />
         {/* <Testing /> */}
       </Switch>
     </div>
