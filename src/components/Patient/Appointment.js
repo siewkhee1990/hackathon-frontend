dad580579eaa0b183728682f9dfb86a7101a0356
Danny Lee <siewkhee1990@hotmail.com>
2021-01-22 23:42:53 +0800

    appointment done

--- src/components/Patient/Appointment.js
+++ src/components/Patient/Appointment.js
@@ -3,103 +3,104 @@ import React, { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { BACKEND_URL_APPOINTMENTS, BACKEND_URL_GPS } from "../Constant";
 
-export default function Appointment() {
-    const [vaccineType, setVaccineType] = useState("Covid-19");
-    const [date, setDate] = useState("");
-    const [nextAppointment, setNextAppointment] = useState("");
-    const [status, setStatus] = useState("");
-    const [remarks, setRemarks] = useState("");
-    const [gps, setGps] = useState([]);
-    const [gpid, setGpid] = useState("");
+export default function Appointment(props) {
+  const [vaccineType, setVaccineType] = useState("Covid-19");
+  const [date, setDate] = useState("");
+  const [gps, setGps] = useState([]);
+  const [gpid, setGpid] = useState("");
 
-    useEffect(() => {
-        axios.get(`${BACKEND_URL_GPS}`).then(response => { setGps(response.data) })
+  useEffect(() => {
+    let token = localStorage.getItem("ptoken");
+    if (!token) {
+      alert("Please login to make an appointment!");
+      props.history.push("/patient");
+    } else {
+      axios.get(`${BACKEND_URL_GPS}`).then((response) => {
+        setGps(response.data);
+      });
+    }
+  }, []);
 
-    }, [])
+  const createAppointment = (event) => {
+    event.preventDefault();
+    if (!date || !vaccineType || !gpid) {
+      alert("Please fill in required field!");
+    } else {
+      let info = JSON.parse(localStorage.getItem("ptoken"));
+      let data = {
+        pid: info.pid,
+        vaccineType: vaccineType,
+        date: date,
+        gpid: gpid,
+      };
+      axios
+        .post(`${BACKEND_URL_APPOINTMENTS}/create`, data)
+        .then((response) => {
+          console.log(response);
+          alert(response.data.message);
+        })
+        .catch((err) => {
+          console.log(err.response);
+          alert(err.response.data.message);
+        });
+    }
+  };
 
+  return (
+    <div>
+      <div>
+        <h2>Book New Appointment</h2>
+        <Form onSubmit={(event) => createAppointment(event)} className="m-3">
+          <Form.Group controlId="vaccination">
+            <Form.Label>Vaccination Type</Form.Label>
+            <Form.Control
+              as="select"
+              value={vaccineType}
+              readOnly
+              onChange={(e) => setVaccineType(e.target.value)}
+            >
+              <option>Covid-19</option>
+              <option>Influenza</option>
+              <option>Hepatitis</option>
+            </Form.Control>
+          </Form.Group>
 
-    const createAppointment = (event) => {
-        event.preventDefault();
-        if (!date || !vaccineType) {
-            alert("Please fill in required field!");
-        } else {
-            let data = {
-                vaccineType: vaccineType,
-                date: date,
-            };
-            axios
-                .post(`${BACKEND_URL_APPOINTMENTS}/create`, data)
-                .then((response) => console.log(response))
-                .catch((err) => console.log(err));
-        }
-    };
+          <Form.Group controlId="date">
+            <Form.Label>Appointment Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Select date..."
+              format="YYYY-Mmm-DD"
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
+              required
+            />
+          </Form.Group>
 
-    return (
-        <div>
-            <div>
-                <h2>Book New Appointment</h2>
-                <Form onSubmit={(event) => createAppointment(event)} className="m-3">
-                    <Form.Group controlId="vaccination">
-                        <Form.Label>Vaccination Type</Form.Label>
-                        <Form.Control
-                            as="select"
-                            value={vaccineType}
-                            readOnly
-                            onChange={(e) => setVaccineType(e.target.value)}
-                        >
-                            <option>Covid-19</option>
-                            <option>Influenza</option>
-                            <option>Hepatitis</option>
-                        </Form.Control>
-                    </Form.Group>
+          <Form.Group controlId="generalpractitioner">
+            <Form.Label>GP</Form.Label>
+            <Form.Control
+              as="select"
+              defaultValue="Choose..."
+              value={gpid}
+              onChange={(e) => setGpid(e.target.value)}
+            >
+              <option>Choose...</option>
+              {gps.map((element) => {
+                return (
+                  <option key={element.gpid} value={element.gpid}>
+                    {element.clinicName}
+                  </option>
+                );
+              })}
+            </Form.Control>
+          </Form.Group>
 
-                    <Form.Group controlId="date">
-                        <Form.Label>Appointment Date</Form.Label>
-                        <Form.Control
-                            type="date"
-                            placeholder="Select date..."
-                            format="YYYY-Mmm-DD"
-                            value={date}
-                            onChange={(event) => setDate(event.target.value)}
-                            required
-                        />
-                    </Form.Group>
-
-                    <Form.Group controlId="generalpractitioner">
-                        <Form.Label>GP</Form.Label>
-                        <Form.Control
-                            as="select"
-                            defaultValue="Choose..."
-                            value={gps} onChange={e => setGps(e.target.value)}>
-                            <option>Choose...</option>
-                            {gps.map((element) => {
-                                return (
-                                    
-                                    <option>{element.clinicName}</option>
-                                )
-                            })}
-                            
-
-
-                        </Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Remarks</Form.Label>
-                        <Form.Control as="textarea" rows={3}
-                            value={remarks} onChange={e => setRemarks(e.target.value)}
-                        />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
-                        Create
-                    </Button>
-                </Form>
-            </div>
-        </div >
-    );
+          <Button variant="primary" type="submit">
+            Create
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
-
-
-
-
