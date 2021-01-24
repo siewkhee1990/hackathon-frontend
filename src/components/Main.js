b1876716b48e02b61d59ce3112d4b6061aeda9fc
VikiBoi <43218183+VikiBoi@users.noreply.github.com>
2021-01-24 16:36:14 +0800

    update v2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Col, Row } from "react-bootstrap";
+import { Button, Container, Col, Row, Card } from "react-bootstrap";
 import Image from 'react-bootstrap/Image';
 import vmslogo from '../,,/../../src/vmslogo.png'; // the ../ is to change the directory!
 
 export default function Main() {
   return (
-    <div >
+    <div style={{ display: 'flex', justifyContent: 'center'}} >
 
-      <Image src={vmslogo} style={{ height: '', width: '720px' }} fluid />
+      <Card style={{ width: '80%' }}>
+        
+        <div style={{ margin: 'auto', width: '50%' }}>
+          <Card.Img variant="top" src={vmslogo} style={{ height: '100%', width: '100%', justifyContent: 'center' }} />
+        </div>
+
+
+        <Card.Body>
+
+          <Container fluid >
+            <Row className="m-3">
+              <Col>
+                <Link to="/patient" style={{ display: 'flex', justifyContent: 'center' }}>
+                  <Button variant="primary" className="mx-2" >
+                    Proceed as Patient
+                  </Button>
+                </Link>
+              </Col>
+
+              <Col>
+                <Link to="/gp" style={{ display: 'flex', justifyContent: 'center' }}>
+                  <Button variant="primary" className="mx-2" >
+                    Proceed as GP
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+          </Container>
+
+        </Card.Body>
+      </Card>
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+      <Image src={vmslogo} style={{ height: '100%', width: '100%' }} fluid />
       <br></br>
       <h4 style={{ display: 'flex', justifyContent: 'center' }}>Welcome to Vaccination Management System! </h4>
       <br></br>
@@ -30,7 +73,7 @@ export default function Main() {
             </Link>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
 
     </div>
   );
